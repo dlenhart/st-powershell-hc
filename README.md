d68f235f7cdae7bd174669e94f96c7fc6b1ed46d
Drew D. Lenhart <dlenhart@gmail.com>
2016-08-30 08:22:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+st-powershell-hc
+===============
+
+My stash of simple powershell scripts to perform health checks on servers or websites.
+
+#Scripts
+
+hc_ping.ps1 - Pings a list of servers or websites from a text file.  Optional failure email.
+
+hc_status_code.ps1 - Gets the http status code from a list of websites in a text file.
+
+#Required
+
+1.  Powershell 2.0+
+
+#Website
+http://snowytech.com/drewlenhart
+
+#License
+MIT License
+
+Copyright (c) 2016 Drew D. Lenhart
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ hc_ping.ps1
@@ -0,0 +1,81 @@
+<#
+
+.SYNOPSIS
+This is a simple Powershell script to ping a list of websites or servers from a text file.
+
+.DESCRIPTION
+The script performs a test-connection on a list of websites/servers to get an up or down status.
+
+.EXAMPLE
+./hc_ping.ps1
+
+.NOTES
+-optional failure email, uncomment function in script.
+
+.LINK
+http://github.com/snowytech/st-powershell-hc
+
+#>
+
+$ServerListFile = "server-ping.txt"   
+$ServerList = Get-Content $ServerListFile -ErrorAction SilentlyContinue
+$LogTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$LogFile = "logs\hc_ping-results.html"
+ 
+##EMAIL function##
+
+function sendMail($datestamp){
+    ##Use GMAILs free smtp services, must use port 587
+    $smtpServer = "smtp.gmail.com" 
+    $srvPort = 587
+    $smtpFrom = "youremail@gmail.com" 
+    $smtpTo = "to email address" 
+    $messageSubject = "Ping Script Results - $datestamp"
+    $message = New-Object System.Net.Mail.MailMessage $smtpfrom, $smtpto 
+    $message.Subject = $messageSubject 
+    $message.IsBodyHTML = $true 
+    $message.Body = "<html><head></head><body>" 
+    $message.Body += Get-Content $LogFile
+    $message.Body += "</body></html>"
+    $smtp = New-Object Net.Mail.SmtpClient($smtpServer, $srvPort)
+    $smtp.EnableSsl = $true
+    $smtp.Credentials = New-Object System.Net.NetworkCredential("gmail username without @gmail.com", "gmail password") 
+    ##Send message
+    $smtp.Send($message)
+}
+
+
+##### Script Starts Here ######  
+$Outputreport = ""
+$Outputreport = "<h3>Ping Stats: $LogTime</h3>"
+$Outputreport += "<table><tr style='background-color: grey; color: white'><td>Hostname</td><td>Status</td></tr>"
+$count = 0
+$failCount = 0
+
+foreach ($Server in $ServerList) {
+
+if (test-Connection -ComputerName $Server -Count 2 -Quiet ) {
+
+    echo "($Server): is pingable"
+    $Outputreport += "<tr><td>$Server</td><td style='background-color: green; color: white'>ONLINE</td></tr>"
+    }
+    else
+    { 
+        echo "$Server seems dead Jim" 
+        $Outputreport += "<tr><td>$Server</td><td style='background-color: red'>OFFLINE</td></tr>"
+        $failCount = $failCount + 1
+    }
+        
+    $count = $count + 1
+        
+}
+
+$Outputreport += "</table>"
+$Outputreport += "<br />Server count: $count"
+echo "Server count: " $count
+$Outputreport | out-file $LogFile
+
+if ($failCount -ge 1) {
+    #Uncomment below to email failures using sendMail ( uses gmail )
+    #sendMail "$LogTime"
+}
